32eb714537fda18a4450e21d34bfa08639689681
zmnatsakan <pmmnacakan11crew@gmail.com>
2023-11-20 13:23:57 +0300

    Initial Commit

---
+++ dices/dicesApp.swift
@@ -0,0 +1,17 @@
+//
+//  dicesApp.swift
+//  dices
+//
+//  Created by mnats on 20.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct dicesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
